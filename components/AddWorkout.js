611c1a43493478920659e2a73606cf7458cc5739
laovirtanen <=>
2024-09-08 18:19:58 +0300

    added fontawesome buttons to buttomgroup

--- components/AddWorkout.js
+++ components/AddWorkout.js
@@ -2,16 +2,41 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from '../styles/AddWorkoutStyles';
 import { ButtonGroup } from 'react-native-elements';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+
 
 const AddWorkout = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [workout, setWorkout] = useState('');
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+
 
   const updateIndex = (index) => {
     setSelectedIndex(index);
   };
 
-  const buttons = ['Run', 'Ski', 'Swim'];
+  const buttons = [
+    { element: () => (
+      <View style={styles.iconButton}>
+        <FontAwesome5 name="swimmer" size={24} color="black" />
+        <Text style={styles.iconText}>Swim</Text>
+      </View>
+    ) },
+    { element: () => (
+      <View style={styles.iconButton}>
+        <FontAwesome5 name="running" size={24} color="black" />
+        <Text style={styles.iconText}>Run</Text>
+      </View>
+    ) },
+    { element: () => (
+      <View style={styles.iconButton}>
+        <FontAwesome5 name="skiing-nordic" size={24} color="black" />
+        <Text style={styles.iconText}>Ski</Text>
+      </View>
+    ) }
+  ];
 
   const handleSubmit = () => {
     console.log(`Workout: ${workout}, Type: ${buttons[selectedIndex]}`);
@@ -29,11 +54,21 @@ const AddWorkout = () => {
         selectedButtonStyle={styles.selectedButtonGroupButton}
       />
 
+
+
       <TextInput
         style={styles.input}
         placeholder="0"
-        value={workout}
-        onChangeText={setWorkout}
+        value={distance}
+        onChangeText={setDistance}
+      />
+
+
+      <TextInput
+        style={styles.input}
+        placeholder="0"
+        value={duration}
+        onChangeText={setDuration}
       />
 
       <TouchableOpacity style={styles.button} onPress={handleSubmit}>
